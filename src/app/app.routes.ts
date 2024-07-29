2b40ebc2d781ab5ee00000e228fd4d1c4725e3ac
satyam sinha <satyamsinha8158@gmail.com>
2024-07-30 00:09:48 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        loadChildren: ()=>import("./pages/login/login.routes").then(route=>route.routes)
+    },
+    {
+        path:'register',
+        loadChildren:()=>import("./pages/register/register.routes").then(route=>route.routes)
+    }
+
+];
